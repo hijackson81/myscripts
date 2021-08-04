44112142adcd6a1953d31c66556946519a6e474f
hijackson81 <gm.jacksonchen@gmail.com>
2021-08-04 10:11:46 +0700

    Veeam Powershell - Decrypt Multiple Disk

---
+++ decrypt_multiple_backup_jobs.ps1
@@ -0,0 +1,15 @@
+﻿Add-PSSnapin VeeamPSSnapin
+
+#Export Encrypted Disk
+Get-VBRImportedEncryptedBackup -Name "Job*" | Select Name | Export-CSV "C:\scripts\encrypted_jobs.csv" -NoTypeInformation -Encoding UTF8
+
+# Import Encrypted Disk Name
+Import-Csv "C:\scripts\encrypted_jobs.csv" | ForEach-Object {
+
+$jobname = $_."Name"
+$encryptedbackup = Get-VBRImportedEncryptedBackup -Name $jobname
+Write-Output "Decrypting - $jobname"
+Set-VBREncryptedBackupPassword -Backup $encryptedbackup -Password "YourEncryptPassword"
+Write-Output "Decrypted Successfully- $jobname"
+}
+
